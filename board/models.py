fc0eb3d8650d406bed851368f375a1c076679100
ryung <kimrung4130@gmail.com>
2017-06-01 20:44:40 +0900

    boafd/models.py Post class created

--- board/models.py
+++ board/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.conf import settings
+from djange.utils import timezone
 # Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length=20)
+	content = models.TextField()
+	author = models.ForeignKey(settings.AUTH_USER_MODEL)
+	created = models.DateTimeField(default=timezone.now())
+	modified = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.title
+		
