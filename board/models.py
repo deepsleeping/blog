7325786414854266394795d98dd1429904179dec
ryung <kimrung4130@gmail.com>
2017-06-01 20:50:36 +0900

    board/models.py typo

--- board/models.py
+++ board/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-from djange.utils import timezone
+from django.utils import timezone
 # Create your models here.
 class Post(models.Model):
 	title = models.CharField(max_length=20)
@@ -11,4 +11,3 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.title
-		
