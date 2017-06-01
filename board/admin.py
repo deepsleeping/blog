6e2866e3166e18ea5733b74431abd4ace7458411
ryung <kimrung4130@gmail.com>
2017-06-01 20:47:52 +0900

    board/admin.py initialized

--- board/admin.py
+++ board/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+	list_display= ['id','title','author','created','modified']
+admin.site.register(Post,PostAdmin)
